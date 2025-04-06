3164cf1dd25aca441377b34531918711438a3a5d
frias122 <57614442+frias122@users.noreply.github.com>
2025-04-06 17:08:09 +0100

    jj

--- Terrafrom/Env/Stage/main.tf
+++ Terrafrom/Env/Stage/main.tf
@@ -2,54 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_cidr" {
-  description = "The CIDR block for the subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "availability_zone" {
-  description = "The availability zone for the instances"
-  type        = string
-  default     = "us-west-2a"
-}
-
-variable "allowed_ssh_cidr" {
-  description = "CIDR block to allow SSH access"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "tags" {
-  description = "Tags to be applied to resources"
-  type        = map(string)
-  default     = {
-    "Name" = "MyInstance"
-  }
-}
-
-variable "instance_groups" {
-  description = "Groups of instances to be created"
-  type = map(object({
-    ami           = string
-    instance_type = string
-    count         = number
-  }))
-  default = {
-    web = {
-      ami           = "ami-0755803bcc58ae721"
-      instance_type = "t2.micro"
-      count         = 2
-    }
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_support = true
@@ -92,16 +44,30 @@ resource "aws_security_group" "allow_ssh" {
   })
 }
 
+# Create a flattened list of instances for each group
+locals {
+  instances = flatten([
+    for group_key, group in var.instance_groups : [
+      for i in range(group.count) : {
+        key         = "${group_key}-${i}"
+        group_key   = group_key
+        instance_id = i
+        ami         = group.ami
+        instance_type = group.instance_type
+      }
+    ]
+  ])
+}
+
 resource "aws_instance" "group_instances" {
-  for_each = var.instance_groups
+  for_each = { for inst in local.instances : inst.key => inst }
 
   ami           = each.value.ami
   instance_type = each.value.instance_type
-  count         = each.value.count
   subnet_id     = aws_subnet.main.id
-  security_group = aws_security_group.allow_ssh.id
+  security_groups = [aws_security_group.allow_ssh.id]
 
   tags = merge(var.tags, {
-    "Name" = "instance-${each.key}"
+    "Name" = "instance-${each.value.group_key}-${each.value.instance_id}"
   })
 }
